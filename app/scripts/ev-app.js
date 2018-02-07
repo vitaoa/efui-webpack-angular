f62325b9ff5ad253e1b1a6b527a244322c1f7b67
vitaoa <jupiterzw@sina.com>
2018-02-07 10:59:32 +0800

    完善首页

--- app/scripts/ev-app.js
+++ app/scripts/ev-app.js
@@ -21,6 +21,20 @@ angular.module('evApp', ['ui.router'])
             })
     }])
 
+    //directive定义
+    .directive('headerpanel', function () {
+        return {
+            restrict: 'EA',
+            templateUrl: 'partials/common/header.html'
+        };
+    })
+    .directive('footerpanel', function () {
+        return {
+            restrict: 'EA',
+            templateUrl: 'partials/common/footer.html'
+        };
+    })
+
     .run(function ($rootScope) {
         console.log($rootScope);
     });
